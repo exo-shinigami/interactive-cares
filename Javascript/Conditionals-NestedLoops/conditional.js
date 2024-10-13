a6054c148060bf96f2949f9277dfd8e243196d6a
exo-shinigami <thegamerzdude@gmail.com>
2024-10-13 20:01:20 +0600

    SECOND COMMIT

---
+++ Javascript/Conditionals-NestedLoops/conditional.js
@@ -0,0 +1,47 @@
+// let date = new Date().getDay();
+
+// if ( date== 0 ) {
+//     console.log('Sunday')
+// } else if ( date == 1) {
+//     console.log('Mondat')
+// } else if ( date == 2) {
+//     console.log('Tuesday')
+// } else if ( date == 3) {
+//     console.log('Wednesday')
+// } else if ( date == 4) {
+//     console.log('Thursday')
+// } else if ( date == 5) {
+//     console.log('Friday')
+// } else  {
+//     console.log('Saturday')
+// }
+
+
+// for (let i = 1; i <= 10; i++) {
+//     for (let j=1; j <= 10; j++) {
+//         console.log('i: ' + i + ' j: ' + j);
+//     }
+// }
+
+// let person = [
+//     ['Setu', 'Manijganj', 93],
+//     ['John', 'Dhaka', 23],
+//     ['Jane', 'England', 34]
+// ];
+
+// for (let row = 0; row < person.length; row++) {
+//     for (let col = 0; col < person[row].length; col++) {
+//         console.log(person[row][col]);
+//     }
+// }
+
+// let rand = Math.round(Math.random() * 100);
+
+// console.log(rand);
+
+// Interview (NO ANSWER)
+// let num = 1;
+// if (--num) {
+//     console.log(num);
+// }
+
